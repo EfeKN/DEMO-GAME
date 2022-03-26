12255ad258e768130c00c4cc1d1b17afd2b5c408
EfeKN <efe.kaan.fidanci@gmail.com>
2022-03-26 22:21:18 +0300

    spritesheet and upper bound added

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,13 +1,28 @@
 package entity;
-
+import main.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Entity {
-      
+
       public int x,y;
       public int speed;
       public int imageNumber;
+      public ArrayList<BufferedImage> images;
+      public BufferedImage spriteSheetImage;
+      public BufferedImageLoader bil;
+      public SpriteSheet spriteSheet;
 
-      public BufferedImage right1,right2;
+      public Entity() {
+            bil = new BufferedImageLoader();
+            try {
+                  spriteSheetImage = bil.loadImage("res/enemy/spritesheet_demo2.png");
+            } catch (IOException e) {
+                  e.printStackTrace();
+            }
+            spriteSheet = new SpriteSheet(spriteSheetImage);
+            images = new ArrayList<BufferedImage>();
+      }
 
 }
